f6092e0463c43b556981d04ecc2e85a614254311
Ilya Erlingas <Aligruit@gmail.com>
2020-05-19 20:47:52 +0300

    adding models

--- work_with_database/phones/models.py
+++ work_with_database/phones/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
+MAX_LENGTH_WORD = 100
 
-class Phone(models.Model):
+
+class Phones(models.Model):
     # TODO: Добавьте требуемые поля
-    pass
+    slug = models.CharField(max_length=MAX_LENGTH_WORD)
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=MAX_LENGTH_WORD)
+    price = models.IntegerField()
+    image = models.ImageField(max_length=MAX_LENGTH_WORD)
+    release_date = models.DateField(max_length=MAX_LENGTH_WORD)
+    lte_exists = models.CharField(max_length=MAX_LENGTH_WORD)
