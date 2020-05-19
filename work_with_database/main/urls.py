dded7f425b3703e5d9b6eac423e86dcfd41ae206
Ilya Erlingas <Aligruit@gmail.com>
2020-05-19 21:04:25 +0300

    updating urls

--- work_with_database/main/urls.py
+++ work_with_database/main/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-import phones.views
+from phones.view import show_catalog, show_product
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', phones.views.show_catalog),
-    path('catalog/<slug:slug>/', phones.views.show_product),
+    path('catalog/', show_catalog, name='catalog'),
+    path('catalog/<slug:slug>/', show_product),
 ]
