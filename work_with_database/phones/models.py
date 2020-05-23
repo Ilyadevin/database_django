3f612cfa92629eae44cf209ea877f87d1982b931
Ilya Erlingas <Aligruit@gmail.com>
2020-05-23 15:29:15 +0300

    right models NOW

--- work_with_database/phones/models.py
+++ work_with_database/phones/models.py
@@ -4,10 +4,10 @@ MAX_LENGTH_WORD = 100
 
 
 class Phones(models.Model):
-    slug = models.CharField(max_length=MAX_LENGTH_WORD)
+    slug = models.SlugField()
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=MAX_LENGTH_WORD)
     price = models.IntegerField()
     image = models.ImageField(max_length=MAX_LENGTH_WORD)
     release_date = models.DateField(max_length=MAX_LENGTH_WORD)
-    lte_exists = models.CharField(max_length=MAX_LENGTH_WORD)
+    lte_exists = models.BooleanField()
