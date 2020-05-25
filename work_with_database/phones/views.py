0d1efa6a28e2f77938778ad9dbb2084fd6c07e1c
Ilya Erlingas <Aligruit@gmail.com>
2020-05-25 18:19:31 +0300

    working catalog

--- work_with_database/phones/views.py
+++ work_with_database/phones/views.py
@@ -6,32 +6,37 @@ def show_catalog(request):
     template = 'catalog.html'
     phone_objects = Phones.objects.all()
     context = {
-        'phones_objects': phone_objects}
-    if request.GET.get('order_by=') == 'by_name':
+        'phones_objects': phone_objects
+    }
+    if request.GET.get('order_by') == 'by_name':
         phone_objects = Phones.objects.all().order_by('name')
-        context = {'phones_objects': phone_objects}
+        context = {
+            'phones_objects': phone_objects
+        }
         return render(request, template, context=context)
-    elif request.GET.get('sort_by_cost_0') is True:
-        return render(request, template, context={
-            'phones_id': Phones.id_p, 'phones_name': Phones.name,
-            'phones_price': sorted(Phones.price), 'phones_image': Phones.image,
-        })
-    elif request.GET.get('sort_by_cost_1') is True:
-        return render(request, template, context={
-            'phones_id': Phones.id_p, 'phones_name': Phones.name,
-            'phones_price': sorted(Phones.price, reverse=True), 'phones_image': Phones.image,
-        })
+    elif request.GET.get('order_by') == 'by_cost_asc':
+        phone_objects = Phones.objects.all().order_by('price')
+        context = {
+            'phones_objects': phone_objects
+        }
+        return render(request, template, context=context)
+    elif request.GET.get('order_by') == 'by_cost_desc':
+        phone_objects = reversed(Phones.objects.all().order_by('price'))
+        context = {
+            'phones_objects': phone_objects
+        }
+        return render(request, template, context=context)
     else:
         return render(request, template, context=context)
 
 
 def show_product(request, slug):
     template = 'product.html'
+    phone_objects = Phones.objects.all()
     context = {
-        'id': Phones.id_p, 'name': Phones.name, 'price': Phones.price, 'image': Phones.image, 'slug': Phones.slug,
-        'lte': Phones.lte_exists, 'date': Phones.release_date,
+        'phones_objects': phone_objects
     }
-    if request.GET.get(slug) == Phones.slug:
+    if request.GET.get(slug) == phone_objects.slug:
         return render(request, template, context=context)
     else:
         return show_catalog(render(request, template, context))
